b60000d079002c7bec31c4267c5f7c8ddc144f26
ManaPHP Mark <manaphp@qq.com>
2020-11-18 18:33:00 +0800

    Modified   .env.example Modified   config/app.php

--- config/app.php
+++ config/app.php
@@ -10,10 +10,10 @@ return [
     'params'     => [],
     'aliases'    => [],
     'components' => [
-        '!rpcServer' => ['port' => 9501, 'worker_num' => 4, 'max_request' => 1000000],
-        'db'         => [env('DB_URL')],
-        'redis'      => [env('REDIS_URL')],
-        'logger'     => ['level' => env('LOGGER_LEVEL', 'info')],
+        'rpcServer' => ['port' => 9501, 'worker_num' => 4, 'max_request' => 1000000],
+        'db'        => [env('DB_URL')],
+        'redis'     => [env('REDIS_URL')],
+        'logger'    => ['level' => env('LOGGER_LEVEL', 'info')],
     ],
     'services'   => [
         'timeService' => 'http://127.0.0.1:9501/time',
@@ -24,5 +24,5 @@ return [
         //'slowlog',
         //'logger',
     ],
-    'tracers'    => ['*']
+    'tracers'    => env('APP_TRACERS', []),
 ];
